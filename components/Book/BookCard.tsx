3b747a2561dccd2195dd002c2e7422e4dd379372
fearhunt <firhanmahdi@gmail.com>
2022-10-29 18:37:34 +0700

    Fixed responsive on card

--- components/Book/BookCard.tsx
+++ components/Book/BookCard.tsx
@@ -16,14 +16,14 @@ const getRandomizedBackgroundGradient = () => {
   return `bg-gradient-to-${randomGradientDirection} from-${randomFirstColor} to-${randomSecondColor}`
 };
 
-const BookCard = (props: Book) => {
+const BookCard = (book: Book) => {
   return (
-    <div className="bg-gradient-to-br from-accent-orange to-accent-red rounded-lg shadow-lg transition-all hover:scale-105 px-8 py-6 mt-24">
+    <div className="bg-gradient-to-br from-accent-orange to-accent-red rounded-lg shadow-lg transition-all hover:scale-105 p-5 md:px-8 md:py-6 mt-20">
       <Image src="https://cdn.sejutacita.id/6138d21e3a09ee0013ee730f/Booku/c55ef13f-eb0e-40de-a04c-e46df5940682.png" alt="Book Cover" width={400} height={600} className="rounded-lg shadow-xl -mt-24 mb-4" />
-      <h5 className="text-xl text-white font-bold">
+      <h5 className="text-md md:text-xl  text-white font-bold">
         The Intelligent Investor
       </h5>
-      <p className="text-white text-sm">
+      <p className="text-white text-xs md:text-sm">
         Benjamin Graham
       </p>
     </div>
